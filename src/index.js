2aaead5bfc8d4eb8fd77dd9e437d71cd35f72884
no-regerts <darraler@gmail.com>
2021-02-10 15:05:28 +0300

    feat: implement min(), max() and avg() array operations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array && array.length === 0) return 0
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array && array.length === 0) return 0
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array && array.length === 0) return 0
+  return array.reduce((a, i) => a + i, 0) / array.length;
 }
